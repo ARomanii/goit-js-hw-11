ac07168d149462b8e293a2ff2f9bc536539ca6dd
Alona <103571003+ARomanii@users.noreply.github.com>
2022-09-29 00:09:42 +0300

    правки

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,17 +1,10 @@
-export const fetchImages = async (inputValue, pageNr) => {
-  return await fetch(
-    `https://pixabay.com/api/?key=30171396-620891811b30796fa2cf3b653&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-  )
-    .then(async response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      return await response.json();
-    })
-    .catch(error => {
-      console.error(error);
-    });
-};
+async function fetchImages() {
+  
+  try {
+    const url = `https://pixabay.com/api/?key=30171396-620891811b30796fa2cf3b653&q=${formRef.elements.searchQuery.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+    const response = await axios.get(url);
+    return response
+  }
+  catch {
+    throw new Error("Что-то пошло не так");
+  }
