53991b41e6865a1f442f10e4317dc836dbab2e28
Alona <103571003+ARomanii@users.noreply.github.com>
2022-09-29 00:11:29 +0300

    ошибка

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,10 +1,11 @@
 async function fetchImages() {
   
   try {
-    const url = `https://pixabay.com/api/?key=30171396-620891811b30796fa2cf3b653&q=${formRef.elements.searchQuery.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+    const url = `https://pixabay.com/api/?key=30188307-c49a871897b6d5bfff07bff1b&q=${formRef.elements.searchQuery.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
     const response = await axios.get(url);
     return response
   }
   catch {
     throw new Error("Что-то пошло не так");
   }
+};
